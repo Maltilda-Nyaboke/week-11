23dc5cade382a4e5930560725cb44610a22b2ccc
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-01 05:28:15 +0300

    style:change init file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,23 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask import Blueprint
+from config import config_options
+
 main = Blueprint('main',__name__)
 from . import views,errors
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__instance_relative_config = True)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from pip import main
 from app.main import app
 
 if __name__ == '__main__':
