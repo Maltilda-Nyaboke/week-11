44bdb2cbe439bcb470e255651d9f51f0d9ac79a7
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-01 05:35:11 +0300

    style:initialize class article

---
+++ app/models/models.py
@@ -0,0 +1,6 @@
+class Article:
+    def __init__(self,title,urlToImage,description,publishedAt):
+        self.title = title
+        self.urlToImage = urlToImage
+        self.description = description
+        self.publishedAt = publishedAt
