1c58a5cb170bb54c128f400eb06d13763fca7bc8
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-01 14:18:13 +0300

    style:rename run file

---
+++ manage.py
@@ -0,0 +1,5 @@
+from pip import main
+from app.main import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- run.py
+++
@@ -1,5 +0,0 @@
-from pip import main
-from app.main import app
-
-if __name__ == '__main__':
-    app.run(debug = True)
