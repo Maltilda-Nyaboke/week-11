4bfc385b9fee67c4be3cd1f618d396627b34fdc9
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-03 19:40:59 +0300

    style:add config files

--- config.py
+++ config.py
@@ -1,38 +1,18 @@
 import os
-
 class Config:
-    '''
-    General configuration parent class
-    '''
-    
-NEWS_API_KEY = 'a10c481237fa4068b0ef87679cdd1490'
-NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q{}-news&apiKey={}'
-
-
-
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q{}-news&apiKey={}'
+    ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    SOURCES_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
-
 
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
-    DEBUG = True
-
-config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
-}
+    
+DEBUG = True
