fd66650511900beaf11313a23e83be585a666644
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-03 19:51:19 +0300

    style:add class article and source

---
+++ app/models/articles.py
@@ -0,0 +1,7 @@
+class Article:
+    def __init__(self, title,image,url, description, postDate):
+        self.title = title
+        self.image = image
+        self.url = url
+        self.description = description
+        self.postDate = postDate    
---
+++ app/models/sources.py
@@ -0,0 +1,8 @@
+class Source:
+    
+    def __init__(self,id,name,description,url,author):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.author = author
